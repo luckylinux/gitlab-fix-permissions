c033fddb32864b839299921e53e3c981b8111167
luckylinux <luckylinux>
2024-06-16 10:18:47 +0200

    Added more Permission Fixes.

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -23,9 +23,29 @@ sudo gitlab-ctl restart
 sudo chown -R "${username}":"${groupname}" /mnt/git/repositories
 sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/uploads
 
+# According to: https://docs.gitlab.com/omnibus/settings/configuration.html#disable-the-varoptgitlab-directory-management
+sudo chown -R "${username}":gitlab-www /var/opt/gitlab/gitlab-rails/shared
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/artifacts
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/external-diffs
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/lfs-objects
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/packages
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/dependency_proxy
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/terraform_state
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/ci_secure_files
+sudo chown -R "${username}":gitlab-www /var/opt/gitlab/gitlab-rails/shared/pages
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/shared/uploads
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-ci/builds
+sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/.ssh
+
 # Based on the Log / Process Analysis detailed above
 sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitlab-rails/sockets/
 sudo chown -R "${username}":"${groupname}" /opt/gitlab/embedded/service/gitlab-rails/log/
 sudo chown -R "${username}":"${groupname}" /var/log/gitlab/puma
 sudo chown -R "${username}":"${groupname}" /opt/gitlab/var/
 sudo chown -R "${username}":"${groupname}" /var/opt/gitlab/gitaly/
+
+# Reconfigure again
+sudo gitlab-ctl reconfigure
+
+# Restart all Services again
+sudo gitlab-ctl restart
